515546483ad3035950b26e7734e212424179423d
ghani <ghani.rizkymail@gmail.com>
2022-06-09 11:29:03 +0700

    interaction test

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const client: any = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-// const rest = new REST({ version: "9" }).setToken(configs.DISCORD_BOT_TOKEN);
+const rest = new REST({ version: "9" }).setToken(configs.DISCORD_BOT_TOKEN);
 
 client.on("ready", async () => {
   console.log(`${client.user.tag} has logged in.`);
@@ -23,7 +23,7 @@ client.on("ready", async () => {
   const channelGame3rb = client.channels.cache.get("881639659577425950");
   const channelCrypto = client.channels.cache.get("905782025565388840");
   const channelCrypto2 = client.channels.cache.get("909033029412995112");
-  // const testGuildId = "908632787874091038"
+  const testGuildId = ["908632787874091038", "285891020720308234"]
   await getList(channelCrypto2);
   setInterval(async () => {
     await game3rbFeed(channelGame3rb);
@@ -31,9 +31,10 @@ client.on("ready", async () => {
     await getList(channelCrypto2);
   }, 600000);
 
-  // await rest.put(Routes.applicationGuildCommands(client.user.id, testGuildId), {
-  //   body: commandsData(),
-  // });
+  await rest.put(Routes.applicationGuildCommands(client.user.id, testGuildId), {
+    body: commandsData(),
+  });
+
 });
 
 client.on("messageCreate", async (msg: any) => {
@@ -52,14 +53,14 @@ client.on("messageCreate", async (msg: any) => {
   if (msg.content.startsWith("g!pray")) pray(msg);
 });
 
-// client.on("interactionCreate", async (interaction : CommandInteraction) => {
-//   if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction : CommandInteraction) => {
+  if (!interaction.isCommand()) return;
 
-//   if (interaction.commandName === "ping")
-//     await interaction.reply("Bot is ready");
+  if (interaction.commandName === "ping")
+    await interaction.reply("Bot is ready");
 
-//   if (interaction.commandName === "prune") prune(interaction.options.getNumber('num'), client, true, interaction)
+  if (interaction.commandName === "prune") prune(interaction.options.getNumber('num'), client, true, interaction)
 
-// });
+});
 
 client.login(configs.DISCORD_BOT_TOKEN);
